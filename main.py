8dc87e0b6d49f6a56b760aca99be360a999563a2
Andrew Flik <41635993+andrewflik@users.noreply.github.com>
2020-07-18 19:44:45 +0530

    Updated the main code
    added one line of code to intiate DB

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/codeblog'
 
 
